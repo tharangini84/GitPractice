0f7d08cc2602e6566313f51bbb3c227eb64837fc
Sudhakar <sudhakar.janne@gmail.com>
2020-06-30 12:46:20 -0700

    added sample method secondcommit

--- src/main/java/GitPractice/GitPractice/GitTest_01.java
+++ src/main/java/GitPractice/GitPractice/GitTest_01.java
@@ -3,4 +3,12 @@ package GitPractice.GitPractice;
 public class GitTest_01 {
 
 	public GitTest_01()
-{System.out.println("GitHub1");}}
+{System.out.println("GitHub1");}
+	
+	
+	public GitTest_01(int a)
+	
+{System.out.println("GitHub1");}
+	
+
+}
